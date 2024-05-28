8fc7219012a104343e1f2eacb989e821e6a2b93e
Jun Quan <jquan0518@gmail.com>
2024-05-28 16:42:57 +0800

    Updated SaveGame.java and Changed all function to static in Script.java

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,8 @@ public class Main {
         CityController controlPanel = new CityController(trainer);
         controlPanel.runCity();
 
+		// SaveGame saveGame = new SaveGame();
+		// saveGame.saveGameMain();
 
 
     }
