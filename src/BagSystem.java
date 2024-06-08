2417d7364f6348e501a2ed516d7322d6d4d3e939
sophist <yunzhezhetion2004@gmail.com>
2024-06-09 00:12:53 +0800

    Solve the bug on Gary, rebuild the attributes of pokemon

--- src/BagSystem.java
+++ src/BagSystem.java
@@ -1,16 +1,20 @@
 import org.w3c.dom.Node;
 
 import java.util.LinkedList;
-
+/*
+This BagSystem is not a necessity. Just  reserve spcae if want to add item system (potion and something like that
+)*/
 public class BagSystem {
     PokemonList pokemonList;
     LinkedList<String> badgeList;
 
+    // Trainer and Enemy will have their own bag, the different is Enemy cant called out their badgeList
     public BagSystem() {
         this.pokemonList = new PokemonList();
         this.badgeList = new LinkedList<>();
     }
 
+    // Use for health all Pokemon inside the bag
     public void healPokemon() {
         for (Pokemon pokemon : pokemonList.list) {
             if (pokemon != null) {
--- src/battleSystem.java
+++ src/battleSystem.java
@@ -1,221 +1,8 @@
 import java.util.*;
-//public class battleSystem {
-//    private final Trainer trainer;
-//    private final Enemy enemy;
-//    private Pokemon trainerCurrentPokemon;
-//    private Pokemon enemyCurrentPokemon;
-//    boolean isWildPokemon;
-//    private final HandingAbnormalInput inputChecker = new HandingAbnormalInput();
-//
-//    public battleSystem(Trainer trainer , Enemy enemy) {
-//        this.trainer = trainer;
-//        this.enemy = enemy;
-//        trainerCurrentPokemon = trainer.trainerBag.pokemonList.list.getFirst();
-//        this.isWildPokemon = enemy instanceof Wild_Pokemon;
-//        startBattle();
-//    }
-//
-//    public void startBattle() {
-//        if (isWildPokemon) {
-//            System.out.println("Battle Start: " + trainer.getName() + " and " + enemy.getName());
-//        }
-//        chooseEnemyPokemon(enemy);
-//
-//        while (trainer.trainerBag.pokemonList.checkStatus() && enemy.enemyBag.pokemonList.checkStatus() && trainer.isKeepStay() && !wildPokemon) {
-//            displayBattleStatus();
-//            if (enemyCurrentPokemon.speed > trainerCurrentPokemon.speed) {
-//                enemyTurn();
-//                if (!trainer.trainerBag.pokemonList.checkStatus()) {
-//                    System.out.println("You are defeated by " + enemy.getName());
-//                    break;
-//                }
-//                trainerTurn();
-////                if (!trainer.isKeepStay())
-////                    break;
-////                if (!enemy.enemyBag.pokemonList.checkStatus()) {
-////                    System.out.println("You are Win!");
-////                    getBadge();
-////                    break;
-////                }
-//            }
-//            else {
-//                trainerTurn();
-//                if (!trainer.isKeepStay())
-//                    break;
-//                if (!enemy.enemyBag.pokemonList.checkStatus()) {
-//                    System.out.println("You are Win!");
-//                    getBadge();
-//                    break;
-//                }
-//                enemyTurn();
-//                if (!trainer.trainerBag.pokemonList.checkStatus()) {
-//                    System.out.println("You are defeated by " + enemy.getName());
-//                    break;
-//                }
-//            }
-//        }
-//    }
-//    public void displayBattleStatus() {
-//        trainerCurrentPokemon.displayPokemonStatus();
-//        enemyCurrentPokemon.displayPokemonStatus();
-//    }
-//    public void chooseEnemyPokemon(Enemy enemy) {
-//        String enemyClass = enemy.getClass().getSimpleName();
-//        switch (enemyClass) {
-//            case "GymLeader", "Gary": {
-//                enemyCurrentPokemon = enemy.enemyBag.pokemonList.get(0);
-//                System.out.printf("%s sends out %s [Level %d] !\n", enemy.getName(), enemyCurrentPokemon.getName(), enemyCurrentPokemon.getLevel());
-//                break;
-//            }
-//            case "Wild_Pokemon": {
-//                int index = new Random().nextInt(enemy.enemyBag.pokemonList.list.size());
-//                Pokemon tmp = enemy.enemyBag.pokemonList.list.get(index);
-//                enemyCurrentPokemon = new Pokemon(tmp.name, tmp.typeString, tmp.HP, tmp.attack, tmp.defense, tmp.speed, tmp.quickMove.getMovesName(), tmp.quickMove.getMovesTypeString()[0], tmp.quickMove.getDPR(), tmp.mainMove.getMovesName(), tmp.mainMove.getMovesTypeString()[0], tmp.mainMove.getDPR());
-//                enemyCurrentPokemon.levelSystem.setDefaultLevel(enemy.getName());
-//                System.out.printf("%s is exploring the wild\n",trainer.getName());
-//                System.out.printf("Suddenly, the %s jumps out!\n",enemyCurrentPokemon.getName());
-//                break;
-//            }
-//            default :
-//                enemyCurrentPokemon = enemy.enemyBag.pokemonList.get(0);
-//                System.out.printf("%s sends out %s [Level %d] !\n", enemy.getName(), enemyCurrentPokemon.getName(), enemyCurrentPokemon.getLevel());
-//
-//        }
-//    }
-//    public void enemyChangePokemon() {
-//        if (enemy.getClass() == Wild_Pokemon.class) {
-//            wildPokemon = true;
-//        }
-//        else {
-//            for (int i = 0; i < enemy.enemyBag.pokemonList.list.size(); i++) {
-//                Pokemon current = enemy.enemyBag.pokemonList.list.get(i);
-//                if (current.getName().equalsIgnoreCase(enemyCurrentPokemon.getName())) {
-//                    enemyCurrentPokemon = enemy.enemyBag.pokemonList.list.get(i + 1);
-//                    break;
-//                }
-//            }
-//        }
-//
-//        System.out.printf("%s sends out %s[Level %d] !", enemy.getName(), enemyCurrentPokemon.getName(), enemyCurrentPokemon.getLevel());
-//    }
-//
-//    public void chooseTrainerPokemon() {
-//        Scanner sc = new Scanner(System.in);
-//        String index = "5070";
-//        while (true) {
-//            System.out.print("Choose Pokemon: ");
-//            index = sc.nextLine();
-//            if (inputChecker.checkAbnormalInput(index, "1", "6")) {
-//                int position = Integer.parseInt(index);
-//                if (trainer.trainerBag.pokemonList.list.get(position-1) == null)
-//                    System.out.println("This pokemon is not available now.");
-//                else if (trainer.trainerBag.pokemonList.get(position-1).HP == 0 )
-//                    System.out.println("That is no pokemon on this place");
-//                else {
-//                    trainerCurrentPokemon = trainer.trainerBag.pokemonList.get(position - 1);
-//                    System.out.printf("%s is sent out!\n", trainerCurrentPokemon.getName());
-//                    trainerCurrentPokemon.getType().typeCounter(trainerCurrentPokemon, enemyCurrentPokemon);
-//                    break;
-//                }
-//            }
-//            else {
-//                System.out.println("Developer: Huh...? I remember I just set 6 position only....");
-//            }
-//        }
-//    }
-//    public void trainerTurn() {
-//        if (trainerCurrentPokemon.HP <= 0) {
-//            trainer.checkAvailablePokemon(trainerCurrentPokemon);
-//            chooseTrainerPokemon();
-//        }
-//        else {
-//            System.out.print("It's your round! ");
-//            System.out.printf("%s's Moves: \n", trainerCurrentPokemon.getName());
-//            System.out.printf("1. %s [%s] (%d/100)\n", trainerCurrentPokemon.getMoveName(0), trainerCurrentPokemon.getMoveType(0), trainerCurrentPokemon.quickMove.QMPoint);
-//            System.out.printf("2. %s [%s] (%d/8)\n", trainerCurrentPokemon.getMoveName(1), trainerCurrentPokemon.getMoveType(1), trainerCurrentPokemon.mainMove.MMPoint);
-//            System.out.println("3. Bag");
-//            System.out.println("4. Escape");
-//            System.out.println("5. Catch");
-//
-//            Scanner sc = new Scanner(System.in);
-//            String input = "5070";
-//            System.out.printf("Which move will %s use?\n", trainerCurrentPokemon.getName());
-//            while (true) {
-//                System.out.print(">>> ");
-//                input = sc.nextLine();
-//                if (inputChecker.checkAbnormalInput(input, "1", "5")) {
-//                    switch (input) {
-//                        case "1":
-//                            trainerCurrentPokemon.useMove(1, enemyCurrentPokemon);
-//                            break;
-//                        case "2":
-//                            trainerCurrentPokemon.useMove(2, enemyCurrentPokemon);
-//                            break;
-//                        case "3":
-//                            trainer.checkAvailablePokemon(trainerCurrentPokemon);
-//                            chooseTrainerPokemon();
-//                            break;
-//                        case "4":
-//                            trainer.tryEscaped();
-//                            break;
-//                        case "5":
-//                            if (enemyCurrentPokemon.master.isEmpty())
-//                                trainer.tryCatch(trainerCurrentPokemon, enemyCurrentPokemon);
-//                            else
-//                                System.out.printf("%s: Don't underestimate the bond between me and my Pokémon! \n", enemy.getName());
-//                            break;
-//                        default:
-//                            System.out.println("Error on battleSystem player turn");
-//                    }
-//                    if (enemyCurrentPokemon.HP <= 0) {
-//                        trainerCurrentPokemon.gainEXP(enemyCurrentPokemon);
-//                    }
-//                    break;
-//                }
-//            }
-//        }
-//
-//    }
-//    public void enemyTurn() {
-//        if (enemy.getClass() == Wild_Pokemon.class && enemyCurrentPokemon.HP<= 0) {
-//            wildPokemon = true;
-//        }
-//        else {
-//            System.out.printf("%s Turn now!", enemy.getName());
-//            if (enemyCurrentPokemon.HP <= 0) {
-//                enemyChangePokemon();
-//            }
-//            else {
-//                int action = new Random().nextInt(3);
-//                switch (action) {
-//                    case 0, 2:
-//                        enemyCurrentPokemon.useMove(1, trainerCurrentPokemon);
-//                        break;
-//                    case 1:
-//                        enemyCurrentPokemon.useMove(2, trainerCurrentPokemon);
-//                        break;
-//                }
-//            }
-//        }
-//
-//
-//    }
-//
-//    public void getBadge() {
-//        String[] leader  = {"Brock", "Misty", "Lt. Surge", "Erika", "Koga", "Sabrina", "Blaine", "Giovanni"};
-//        String[] badgeList = {"Boulder Badge", "Cascade Badge", "Thunder Badge", "Rainbow Badge", "Soul Badge", "Marsh Badge", "Volcano Badge", "Earth Badge"};
-//        if (enemy.getClass() == GymLeader.class) {
-//            for (int i=0; i< leader.length; i++) {
-//                if (enemy.name.equalsIgnoreCase(leader[i])) {
-//                    trainer.trainerBag.badgeList.add(badgeList[i]);
-//                    System.out.printf("%s get %s!", trainer.getName(), badgeList[i]);
-//                }
-//            }
-//        }
-//    }
-//
-//
-//}
+
+/*
+This Battle System will is creating a new condition for Trainer and his enemy to fight
+ */
 public class battleSystem {
     private final Trainer trainer;
     private final Enemy enemy;
@@ -224,11 +11,12 @@ public class battleSystem {
     private boolean isWildPokemon, catched, keepBattle;
     private final HandingAbnormalInput inputChecker = new HandingAbnormalInput();
 
+    // Everytime when trainer match will an enemy, new battle start ( all the boolean condition use for ensure the battle is continued will be reset)
     public battleSystem(Trainer trainer, Enemy enemy) {
         this.trainer = trainer;
         this.enemy = enemy;
         this.trainerCurrentPokemon = trainer.trainerBag.pokemonList.list.get(0);
-        this.isWildPokemon = enemy instanceof Wild_Pokemon;
+        this.isWildPokemon = enemy instanceof Wild_Pokemon;     // Check the enemy is wild pokemon or not, Wild pokemon will have diff battle logic with GymLeader and Gary
         this.catched = false;
         this.keepBattle = true;
         this.startBattle();
@@ -359,10 +147,18 @@ public class battleSystem {
                 if (inputChecker.checkAbnormalInput(input, "1", "5")) {
                     switch (input) {
                         case "1":
-                            trainerCurrentPokemon.useMove(1, enemyCurrentPokemon);
+                            if (trainerCurrentPokemon.quickMove.QMPoint <=0 ) {
+                                System.out.println("It doesn't have enough power to use this move!");
+                            }
+                            else
+                                trainerCurrentPokemon.useMove(1, enemyCurrentPokemon);
                             break;
                         case "2":
-                            trainerCurrentPokemon.useMove(2, enemyCurrentPokemon);
+                            if (trainerCurrentPokemon.mainMove.MMPoint <= 0) {
+                                System.out.println("It doesn't have enough power to use this move!");
+                            }
+                            else
+                                trainerCurrentPokemon.useMove(2, enemyCurrentPokemon);
                             break;
                         case "3":
                             trainer.checkAvailablePokemon(trainerCurrentPokemon);
